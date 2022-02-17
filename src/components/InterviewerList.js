6c25b4eaaa9ddfb9e664b933b4bfbac4410a5b4d
lenilsamuel <lenilvarghese@gmail.com>
2022-02-16 19:44:08 -0700

    Add InterviewerList component, style it and pass props to render data

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList(props) {
+
+  const allInterviewers = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem 
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected = {interviewer.id === props.interviewer}
+        setInterviewer={props.setInterviewer}
+      />
+    )
+  })
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{allInterviewers}</ul>
+    </section>
+  );
+}
