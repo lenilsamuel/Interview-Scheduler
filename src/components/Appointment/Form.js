29f33b51335f06161e3e6eb6ff14233ce8e978ff
lenilsamuel <lenilvarghese@gmail.com>
2022-02-18 11:22:46 -0700

    Add state to Form component and reset functionality to Cancel button

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  function reset() {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  function cancel() {
+    reset();
+    return props.onCancel;
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-          This must be a controlled component
-          your code goes here
-        */
+            value={props.student}
+            onChange={(event) => setStudent(event.target.value)}
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} />
+        <InterviewerList
+          interviewers={props.interviewers}
+          onChange={setInterviewer}
+          value={interviewer}
+        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>
+          <Button danger onClick={cancel}>
             Cancel
           </Button>
           <Button confirm onClick={props.onSave}>
