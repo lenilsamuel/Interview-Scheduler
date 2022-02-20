1ed56461fcc5fa8093bb35dcbe9ad7eda9390efb
lenilsamuel <lenilvarghese@gmail.com>
2022-02-19 18:24:44 -0700

    Add test data for appointments and pass to Appointment component to render list of appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,47 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
+import "components/Appointment";
+import Appointment from "components/Appointment";
+
+const appointments = {
+  1: {
+    id: 1,
+    time: "12pm",
+  },
+  2: {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  3: {
+    id: 3,
+    time: "2pm",
+  },
+  4: {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+  5: {
+    id: 5,
+    time: "4pm",
+  },
+};
 
 const days = [
   {
@@ -20,11 +61,19 @@ const days = [
   },
 ];
 
-
-
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
 
-  const [day, setDay] = useState('Monday')
+  const appointmentsArr = Object.values(appointments);
+
+  const appointmentData = appointmentsArr.map((appointment) => {
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+      />
+    );
+  });
 
   return (
     <main className="layout">
@@ -36,11 +85,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList
-            days={days}
-            value={day}
-            onChange={setDay}
-          />
+          <DayList days={days} value={day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -49,7 +94,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentData}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
