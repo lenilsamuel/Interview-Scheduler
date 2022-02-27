45d03f5a62d2bb07c959623177103b82937c470e
lenilsamuel <lenilvarghese@gmail.com>
2022-02-26 18:34:01 -0700

    Add useVisualMode to Appointment component and conditionally render Empty/Show components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,12 +3,26 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
-      <Header time={props.time}/>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+      <Header time={props.time} />
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
     </article>
   );
 }
