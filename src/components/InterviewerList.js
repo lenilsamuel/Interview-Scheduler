4a90cbb7929c13dbbeb8d254241e184a471ad43c
lenilsamuel <lenilvarghese@gmail.com>
2022-03-20 11:20:11 -0600

    Add prop-types library

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,26 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
-
   const allInterviewers = props.interviewers.map((interviewer) => {
     return (
-      <InterviewerListItem 
+      <InterviewerListItem
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected = {interviewer.id === props.value}
-        setInterviewer={(() => props.onChange(interviewer.id))}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
-    )
-  })
-
+    );
+  });
+  
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">{allInterviewers}</ul>
     </section>
   );
+
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
